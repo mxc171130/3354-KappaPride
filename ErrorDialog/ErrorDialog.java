aad1862951c5ea35bd9ce7caf09c87ca41cc6d87
Alazar Debello <ahd170130@utdallas.edu>
2019-11-01 12:29:28 -0500

    Commiting ErrorDialog class and its activity

---
+++ ErrorDialog/ErrorDialog.java
@@ -0,0 +1,9 @@
+package com.example.dialog;
+
+public class ErrorDialog {
+    private String content="Error has Occurred, Now Closing App";
+
+    public String getContent() {
+        return content;
+    }
+}
