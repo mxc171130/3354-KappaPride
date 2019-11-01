c4615086b864e13fba0e7804dd80cc2a1d3d3d5e
Alazard <54555293+Alazard@users.noreply.github.com>
2019-11-01 12:31:41 -0500

    Delete ErrorDialog.java

--- ErrorDialog.java
+++
@@ -1,9 +0,0 @@
-package com.example.dialog;
-
-public class ErrorDialog {
-    private String content="Error has Occurred, Now Closing App";
-
-    public String getContent() {
-        return content;
-    }
-}
