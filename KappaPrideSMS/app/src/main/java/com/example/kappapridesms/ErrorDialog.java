9d49b5ee7c2f8dd91e3138db5bcf37fac1631056
Alazar Debello <ahd170130@utdallas.edu>
2019-11-20 15:23:19 -0600

    Placing ErrorDialog

--- KappaPrideSMS/app/src/main/java/com/example/kappapridesms/ErrorDialog.java
+++ KappaPrideSMS/app/src/main/java/com/example/kappapridesms/ErrorDialog.java
@@ -1,9 +1,34 @@
-package com.example.kappapridesms;
+package com.example.errordialog;
 
-public class ErrorDialog {
-    private String content="Error has Occurred, Now Closing App";
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 
-    public String getContent() {
-        return content;
+import androidx.fragment.app.DialogFragment;
+
+public class ErrorDialog extends DialogFragment
+{
+    private String content="Message cannot be sent";
+    public static ErrorDialog newInstance()
+    {
+        ErrorDialog frag=new ErrorDialog();
+        Bundle args= new Bundle();
+        frag.setArguments(args);
+        return frag;
+
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
+        builder.setTitle("ERROR").setMessage(content).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dismiss();
+            }
+        });
+
+        return builder.create();
     }
 }
