dafc5e63296cc1f9d136f1323fa0ebb9598156ed
mxc171130 <mxc171130@utdallas.edu>
2019-11-29 00:29:50 -0600

    Layout fixed. Don't think a menu button is necessary for this activity given the fact that Nathan's branch includes a menu item that adds to the blacklist. This activity may just be used to browse the blacklisted contacts.

--- KappaPrideSMS/app/src/main/java/com/example/kappapridesms/BlacklistActivity.java
+++ KappaPrideSMS/app/src/main/java/com/example/kappapridesms/BlacklistActivity.java
@@ -2,14 +2,30 @@ package com.example.kappapridesms;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class BlacklistActivity extends Activity {
+public class BlacklistActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.blacklist_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.blacklist_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.blacklist_menu, menu);*/
+        getMenuInflater().inflate(R.menu.blacklist_menu, menu);
+        return true;
     }
 }
