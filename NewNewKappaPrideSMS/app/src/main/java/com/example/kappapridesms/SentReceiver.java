c9e69e5928c1d6029afd4879f6d50f4bb697fa52
mxs137330 <mohammadshalabi1995@gmail.com>
2019-12-03 20:41:13 -0600

    Worked on documentation of the code
    Commented MessageViewAdapter and SentReceiver

--- NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/SentReceiver.java
+++ NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/SentReceiver.java
@@ -5,6 +5,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+/**
+ * Deals with receiving messages. It extends the BroadcastReceiver.
+ * It contains a public interface OnFailedSendListener.
+ * <p>
+ *     Has one private attribute:
+ *     <p>
+ *         - OnFailedSendListener m_onFailedSendListener
+ *     </p>
+ *     Has two public methods:
+ *     <p>
+ *         - public void onReceive(Context context, Intent intent)
+ *         - public SentReceiver(OnFailedSendListener listener)
+ *     </p>
+ *
+ * </p>
+ */
 public class SentReceiver extends BroadcastReceiver
 {
     private OnFailedSendListener m_onFailedSendListener;
@@ -15,6 +31,14 @@ public class SentReceiver extends BroadcastReceiver
     }
 
     @Override
+    /**
+     * Overriden onReceive method. Checks if the
+     * message was received. If the message was not
+     * received then it displays the dialog.
+     *
+     * @param context handle for resolving issues
+     * @param intent facilitates communication between Components
+     */
     public void onReceive(Context context, Intent intent)
     {
         if(intent.getAction().equals("SMS_SENT"))
@@ -28,6 +52,13 @@ public class SentReceiver extends BroadcastReceiver
         }
     }
 
+    /**
+     * Assigns the OnFailedSendLister to the private
+     * attribute m_onFailedSendListener.
+     *
+     * @param listener a failed send listener that listens
+     *                 if a msg has failed
+     */
     public SentReceiver(OnFailedSendListener listener)
     {
         m_onFailedSendListener = listener;
