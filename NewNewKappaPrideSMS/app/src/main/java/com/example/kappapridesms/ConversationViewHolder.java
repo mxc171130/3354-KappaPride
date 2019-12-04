8b39d5f65fa1140396302350632881da2aaab7f8
mxs137330 <mohammadshalabi1995@gmail.com>
2019-12-03 21:14:20 -0600

    Commented the ConversationViewHolder
    About 75% of the classes have been documented as of now. The rest of the classes we are still working on.

--- NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/ConversationViewHolder.java
+++ NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/ConversationViewHolder.java
@@ -7,18 +7,48 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-
+/**
+ * ViewHolder that deals the UI in the ConversationActivity.
+ * This class extends the RecyclerView.ViewHolder.
+ * <p>
+ *     Has one private attribute:
+ *     <p>
+ *         - LinearLayout m_conversationBubble
+ *     </p>
+ *     Has one overloaded constructor:
+ *     <p>
+ *         - public ConversationViewHolder(@NonNull LinearLayout conversationBubble)
+ *     </p>
+ *     Has one public method:
+ *     <p>
+ *         - public LinearLayout getConversationBubble()
+ *     </p>
+ * </p>
+ *
+ */
 public class ConversationViewHolder extends RecyclerView.ViewHolder
 {
-
     private LinearLayout m_conversationBubble;
 
+    /**
+     * Constructs an instance of a ConversationViewHolder. It is
+     * passed a conversationBubble of LinearLayout. It then calls
+     * its super class constructor and then assigns the conversationBubble
+     * to the private member of the class
+     *
+     * @param conversationBubble conversation bubble in the ConversationActivity
+     */
     public ConversationViewHolder(@NonNull LinearLayout conversationBubble)
     {
         super(conversationBubble);
         m_conversationBubble = conversationBubble;
     }
 
+    /**
+     * Returns the conversation bubble
+     *
+     * @return m_conversationBubble
+     */
     public LinearLayout getConversationBubble()
     {
         return m_conversationBubble;
