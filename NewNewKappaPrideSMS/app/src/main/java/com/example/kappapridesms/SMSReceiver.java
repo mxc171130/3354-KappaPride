48be35a14d021e3dc80ebe04bfcd9411964cbe0d
Alazar Debello <ahd170130@utdallas.edu>
2019-12-03 19:48:41 -0600

    Comments

--- NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/SMSReceiver.java
+++ NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/SMSReceiver.java
@@ -25,29 +25,41 @@ public class SMSReceiver extends BroadcastReceiver
 {
     public static final String NOTIFICATION = "kappa_notification";
 
+    /**
+     *
+     * @param context
+     * @param intent the intent is the SMS message that will be sent
+     *This program runs in the background and whenever a message is sent to the phone it gets called.
+     *The program then checks to see if the number is black listed,if not then the phone will get the message
+     * and the phone will send the user a notification
+     */
+
     @Override
     public void onReceive(Context context, Intent intent)
-    {
+    {   String userNumber="";
+        //Checks to see if the intent being sent is a SMS message
         if(intent.getAction().equals(SMS_RECEIVED_ACTION))
-        {
+        {   //The message is then placed in an array of SMSmessage,as well as the blacklist numbers
             SmsMessage[] messages = getMessagesFromIntent(intent);
             ConversationRepository instance = ConversationRepository.getInstance();
             Blacklist blacklist = instance.getBlacklist();
-
+            //for each message the phone will check the date,if the message is self sent
+            //and the actual message itself
             nextMessage:
             for(SmsMessage message : messages)
             {
                 Message receivedMessage = new Message(new Date().getTime(), false, message.getMessageBody());
 
                 String senderPhoneNumber = message.getOriginatingAddress();
-
+                userNumber=senderPhoneNumber;
+                //Checks if the phonenumber is of valid length
                 if(senderPhoneNumber.length() == 10)
                 {
                     senderPhoneNumber = "1" + senderPhoneNumber;
                 }
-
+                //converts number into Long
                 long senderPhoneNumberLong = Long.parseLong(senderPhoneNumber);
-
+                //Checks to see if the phone number is blacklisted
                 for(int i = 0; i < blacklist.size(); i++)
                 {
                     if(senderPhoneNumberLong == blacklist.getBlacklistedContact(i).getPhoneNumber())
@@ -55,7 +67,7 @@ public class SMSReceiver extends BroadcastReceiver
                         continue nextMessage;
                     }
                 }
-
+                //Places the message in the conversation
                 for(Conversation insertConversation : instance.getConversations())
                 {
                     if(insertConversation.getRecipientPhone() == senderPhoneNumberLong)
@@ -70,28 +82,28 @@ public class SMSReceiver extends BroadcastReceiver
                 instance.addConversation(newConversation);
                 newConversation.addMessage(receivedMessage);
             }
-
+            //Saves Conversation in phones memory
             FileSystem.getInstance().saveConversations(instance.getConversations());
-
+            //Creating Notification
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            {
+            {    //First Creating Notifcation Channel
                 NotificationManager m_manger = context.getSystemService(NotificationManager.class);
 
                 NotificationChannel popUp = new NotificationChannel(NOTIFICATION, "popUp", NotificationManager.IMPORTANCE_DEFAULT);
                 popUp.setDescription("SMS notification");
                 m_manger.createNotificationChannel(popUp);
             }
-
+            //Places the settings for the notification
             Notification m_notification= new NotificationCompat.Builder(context, NOTIFICATION)
                     .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setContentTitle("Message")
+                    .setContentTitle(userNumber)
                     .setContentText("Has sent you a message")
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
                     .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                     .setChannelId(NOTIFICATION)
                     .setAutoCancel(true)
                     .build();
-
+            //Displays Notification
             NotificationManagerCompat displayManager = NotificationManagerCompat.from(context);
             displayManager.notify(15234,m_notification);
         }
