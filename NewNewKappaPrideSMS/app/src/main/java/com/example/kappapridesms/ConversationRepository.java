46641df2b360fc47c1ad5cbcff346c3ed9b47e6b
Nathan Beck <nab170130@utdallas.edu>
2019-12-03 19:09:43 -0600

    Commenting
    Added some comments and javadoc to the code

--- NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/ConversationRepository.java
+++ NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/ConversationRepository.java
@@ -2,57 +2,121 @@ package com.example.kappapridesms;
 
 import java.util.*;
 
+/**
+ * ConversationRepository functions as a central hub for all the message data,
+ * conversation data, contact data, and blacklist data. Users of this class
+ * retrieve a singleton instance through the getInstance() method. Hence, all
+ * modifications to the data are reflected in every retrieved instance since
+ * there is only one instance.
+ *
+ * @author Nathan Beck
+ */
 public class ConversationRepository
 {
+    /**
+     * The sole instance of ConversationRepository, retrieved through getInstance()
+     */
     private static final ConversationRepository m_instance = new ConversationRepository();
 
+    /**
+     * The active conversations, holding all message data and conversation data
+     */
     private ArrayList<Conversation> m_conversations;
+
+    /**
+     * The target conversation to be displayed and queried for message additions
+     */
     private Conversation m_targetConversation;
+
+    /**
+     * The manager for all the contacts, holding all the contact data
+     */
     private ContactManager m_contactManager;
+
+    /**
+     * The blacklist, holding all blacklisted contacts
+     */
     private Blacklist m_blacklist;
 
 
+    /**
+     * Adds a new conversation to the central repository.
+     *
+     * @param addConversation The conversation to be added
+     */
     public void addConversation(Conversation addConversation)
     {
         m_conversations.add(addConversation);
     }
 
 
+    /**
+     * Returns the sole instance of ConversationRepository.
+     *
+     * @return The sole instance of ConversationRepository.
+     */
     public static ConversationRepository getInstance()
     {
         return m_instance;
     }
 
 
+    /**
+     * Retrieves the blacklist of this ConversationRepository
+     *
+     * @return The blacklist of this ConversationRepository
+     */
     public Blacklist getBlacklist()
     {
         return m_blacklist;
     }
 
 
+    /**
+     * Retrieves all conversations of this ConversationRepository
+     *
+     * @return All Conversations held by this ConversationRepository
+     */
     public ArrayList<Conversation> getConversations()
     {
         return m_conversations;
     }
 
 
+    /**
+     * Retrieves the ContactManager of this ConversationRepository
+     *
+     * @return The ContactManager of this ConversationRepository
+     */
     public ContactManager getContactManager()
     {
         return m_contactManager;
     }
 
 
+    /**
+     * Retrieves the current targeted conversation of this ConversationRepository
+     *
+     * @return The targeted conversation of this ConversationRepository
+     */
     public Conversation getTargetConversation()
     {
         return m_targetConversation;
     }
 
 
+    /**
+     * Loads any conversation data present in the file system using FileSystem.
+     */
     public void loadConversations()
     {
+        // Load the conversations into a new ArrayList
         ArrayList<Conversation> loadedConversations = new ArrayList<Conversation>();
         FileSystem.getInstance().loadConversations(loadedConversations);
 
+        // Scan through the new ArrayList. Update the target conversation to the conversation in the new
+        // ArrayList with the matching number, also setting the conversations to the loaded ArrayList if
+        // successful.
         for(int i = 0; i < loadedConversations.size(); i++)
         {
             if(loadedConversations.get(i).getRecipientPhone() == getTargetConversation().getRecipientPhone())
@@ -65,11 +129,21 @@ public class ConversationRepository
     }
 
 
+    /**
+     * Sets the target conversation to that specified by the passed index into the conversation ArrayList
+     * of this ConversationRepository.
+     *
+     * @param index The index of this ConversationRepository's conversations ArrayList in which to select
+     *              the new target conversation
+     */
     public void setTargetConversation(int index)
     {
         m_targetConversation = m_conversations.get(index);
     }
 
+    /**
+     * Creates the sole instance of ConversationRepository, initializing its fields
+     */
     private ConversationRepository()
     {
         m_conversations = new ArrayList<Conversation>();
--- NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/FileSystem.java
+++ NewNewKappaPrideSMS/app/src/main/java/com/example/kappapridesms/FileSystem.java
@@ -9,25 +9,49 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+/**
+ * This class implements all of the I/O operations performed by this application.
+ * All modifications to ConversationRepository's fields should be handled by the
+ * appropriate save feature implemented in this class. This class uses the Singleton
+ * design pattern to ensure that only one FileSystem is in use.
+ *
+ * @author Nathan Beck
+ */
 public class FileSystem
 {
+    /**
+     * The sole instance of FileSystem
+     */
     private static FileSystem instance = new FileSystem();
 
+    /**
+     * Returns the sole instance of FileSystem
+     *
+     * @return The sole instance of FileSystem
+     */
     public static FileSystem getInstance()
     {
         return instance;
     }
 
+    /**
+     * Returns an array of phone numbers that are present in the file system.
+     *
+     * @return An ArrayList of phone numbers present in the saved blacklist
+     */
     public ArrayList<Long> loadBlacklistNumbers()
     {
         try
         {
+            // Get this application's path to its data storage
             Context applicationContext = KappaApplication.getAppContext();
             StringBuilder pathBuilder = new StringBuilder();
 
+            // Begin building the path to the blacklist
             pathBuilder.append(applicationContext.getFilesDir().getCanonicalPath());
             pathBuilder.append("/contacts/blacklist");
 
+            // Create a file to open the blacklist
             File blacklistFile = new File(pathBuilder.toString());
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(blacklistFile)));
@@ -35,6 +59,7 @@ public class FileSystem
 
             ArrayList<Long> addedNumbers = new ArrayList<Long>();
 
+            // Read each number into the ArrayList of blacklisted numbers
             while((line = bufferedReader.readLine()) != null)
             {
                 addedNumbers.add(Long.parseLong(line));
@@ -52,26 +77,36 @@ public class FileSystem
         return null;
     }
 
+    /**
+     * Loads all contacts saved to the file system into the passed ContactManager.
+     *
+     * @param contactManager The ContactManager in which to load the contacts
+     */
     public void loadContacts(ContactManager contactManager)
     {
         try
         {
+            // Get the path of this application's data
             Context applicationContext = KappaApplication.getAppContext();
             StringBuilder pathBuilder = new StringBuilder();
 
+            // Begin building the path to the contacts file
             pathBuilder.append(applicationContext.getFilesDir().getCanonicalPath());
             pathBuilder.append("/contacts/contacts");
 
+            // Create the file to open the contacts file
             File contactsFile = new File(pathBuilder.toString());
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(contactsFile)));
             String line;
 
+            // Read the number and contact information until the end of file
             while((line = bufferedReader.readLine()) != null)
             {
                 long number = Long.parseLong(line);
                 line = bufferedReader.readLine();
 
+                // Add the contact to the contact manager
                 contactManager.addContact(number, line);
             }
 
@@ -83,24 +118,38 @@ public class FileSystem
         }
     }
 
+    /**
+     * Loads the conversations present in the file system into the passed ArrayList.
+     *
+     * @param conversations The ArrayList in which to load the conversations
+     */
     public void loadConversations(ArrayList<Conversation> conversations)
     {
         try
         {
+            // Get the root path associated with this application's data
             Context applicationContext = KappaApplication.getAppContext();
             StringBuilder pathBuilder = new StringBuilder();
 
+            // Begin to construct the conversations directory
             pathBuilder.append(applicationContext.getFilesDir().getCanonicalPath());
             pathBuilder.append("/conversations/");
 
+            // Open the directory and retrieve all files in the directory.
+            // Each file in this directory is a conversation, which is a directory that
+            // houses each message in the conversation. The name of the file in each
+            // conversation directory is the timestamp of the message while its contents
+            // are the actual message content to be displayed.
             File conversationPath = new File(pathBuilder.toString());
             File[] allConversations = conversationPath.listFiles();
 
             for(int i = 0; i < allConversations.length; i++)
             {
+                // Create a conversation based on the name of the file (which houses the phone number)
                 String recipientPhone = allConversations[i].getName();
                 Conversation addConversation = new Conversation(Long.parseLong(recipientPhone));
 
+                // Obtain all messages in the conversation
                 File[] allMessages = allConversations[i].listFiles();
 
                 for(int j = 0; j < allMessages.length; j++)
@@ -108,11 +157,13 @@ public class FileSystem
                     // Obtain a BufferedReader to read each message
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(allMessages[j])));
 
+                    // Determine if the message was sent from this phone by reading the first line
                     boolean sentFromThisPhone = bufferedReader.readLine().equals("true");
 
                     StringBuilder contentBuilder = new StringBuilder();
                     String line;
 
+                    // Obtain the message content by reading each line thereafter
                     while((line = bufferedReader.readLine()) != null)
                     {
                         contentBuilder.append(line);
@@ -120,6 +171,7 @@ public class FileSystem
 
                     bufferedReader.close();
 
+                    // Obtain the timestamp by reading the name
                     long timestamp = Long.parseLong(allMessages[j].getName());
 
                     // Recreate the message object and add it to the conversation
@@ -127,6 +179,7 @@ public class FileSystem
                     addConversation.addMessage(addMessage);
                 }
 
+                // Add the newly constructed conversation to the passed ArrayList
                 conversations.add(addConversation);
             }
         }
@@ -136,6 +189,11 @@ public class FileSystem
         }
     }
 
+    /**
+     * Saves each conversation in the passed ArrayList to the file system.
+     *
+     * @param conversations The ArrayList of conversations to be saved
+     */
     public void saveConversations(ArrayList<Conversation> conversations)
     {
         try
@@ -146,6 +204,7 @@ public class FileSystem
             {
                 Conversation targetConversation = conversations.get(i);
 
+                // Create the directory in which to save this conversation
                 StringBuilder directoryBuilder = new StringBuilder();
                 directoryBuilder.append(applicationContext.getFilesDir().getCanonicalPath());
                 directoryBuilder.append("/conversations/");
@@ -159,6 +218,7 @@ public class FileSystem
                     directoryTestFile.mkdirs();
                 }
 
+                // Remove all stale data from this directory
                 File[] cleanArrayFiles = directoryTestFile.listFiles();
 
                 for(File deleteFile : cleanArrayFiles)
@@ -166,6 +226,7 @@ public class FileSystem
                     deleteFile.delete();
                 }
 
+                // Save the individual conversation
                 saveConversation(directoryBuilder.toString(), targetConversation);
             }
         }
@@ -175,12 +236,20 @@ public class FileSystem
         }
     }
 
+
+    /**
+     * Saves the passed conversation to the passed directory.
+     *
+     * @param directory The directory in which to save the conversation
+     * @param conversation The conversation to save
+     */
     private void saveConversation(String directory, Conversation conversation)
     {
         try
         {
             for (int j = 0; j < conversation.size(); j++)
             {
+                // Get the message content and create the file in which to save
                 Message saveMessage = conversation.getMessage(j);
 
                 StringBuilder fileBuilder = new StringBuilder();
@@ -189,6 +258,7 @@ public class FileSystem
 
                 PrintWriter printWriter = new PrintWriter(new File(fileBuilder.toString()));
 
+                // Print message information
                 if(saveMessage.isSentFromThisPhone())
                 {
                     printWriter.println("true");
@@ -209,13 +279,20 @@ public class FileSystem
         }
     }
 
+    /**
+     * Saves the passed ArrayList of contacts to the file system.
+     *
+     * @param contacts The ArrayList of contacts to be saved
+     */
     public void saveContacts(ArrayList<Contact> contacts)
     {
         try
         {
+            // Get the root path of this application's data
             Context applicationContext = KappaApplication.getAppContext();
             StringBuilder pathBuilder = new StringBuilder();
 
+            // Construct the contacts path
             pathBuilder.append(applicationContext.getFilesDir().getCanonicalPath());
             pathBuilder.append("/contacts/contacts");
 
@@ -223,6 +300,7 @@ public class FileSystem
 
             PrintWriter printWriter = new PrintWriter(contactsFile);
 
+            // Write each contact into the file
             for(int i = 0; i < contacts.size(); i++)
             {
                 Contact saveContact = contacts.get(i);
@@ -239,13 +317,20 @@ public class FileSystem
         }
     }
 
+    /**
+     * Saves the blacklisted numbers to the file system.
+     *
+     * @param blacklist The blacklist to save
+     */
     public void saveBlacklistNumbers(Blacklist blacklist)
     {
         try
         {
+            // Get the root path of this application's data
             Context applicationContext = KappaApplication.getAppContext();
             StringBuilder pathBuilder = new StringBuilder();
 
+            // Finish building the path to the blacklist
             pathBuilder.append(applicationContext.getFilesDir().getCanonicalPath());
             pathBuilder.append("/contacts/blacklist");
 
@@ -253,6 +338,7 @@ public class FileSystem
 
             PrintWriter printWriter = new PrintWriter(blacklistFile);
 
+            // Write the blacklisted numbers to the file system
             for(int i = 0; i < blacklist.size(); i++)
             {
                 Contact saveNumberFromContact = blacklist.getBlacklistedContact(i);
@@ -268,10 +354,14 @@ public class FileSystem
         }
     }
 
+    /**
+     * Creates the sole instance of FileSystem
+     */
     private FileSystem()
     {
         try
         {
+            // Create the contacts folder if it doesn't exist upon initialization
             Context applicationContext = KappaApplication.getAppContext();
             StringBuilder pathBuilder = new StringBuilder();
 
