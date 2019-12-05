68ce2a8e4781620847516596e6ad1a3c7e817544
mxc171130 <mxc171130@utdallas.edu>
2019-12-04 20:29:18 -0600

    Added the javadoc to the message test.

--- NewNewKappaPrideSMS/app/src/test/java/com/example/kappapridesms/MessageTest.java
+++ NewNewKappaPrideSMS/app/src/test/java/com/example/kappapridesms/MessageTest.java
@@ -6,39 +6,57 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Test class for the Message class. This tests the timestamp, content, sentFromPhone, and the year.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @author Michael Chang
  */
 public class MessageTest {
+    /**
+     * This initializes the initial object of class Message.
+     */
     private Message message;
-    //private Message testMessage1 = new Message(11111111, true, "This is the first test message.");
-    //private Message testMessage2 = new Message(22222222, true, "This is the second test message.");
 
+    /**
+     * This method initializes the values before the actual testing happens.
+     *
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception
     {
         message = new Message(11111111, true, "This is the test message.");
     }
 
+    /**
+     * This method will test if the timestamps are correct.
+     */
     @Test
     public void timestampTest()
     {
         assertEquals(11111111, message.getTimestamp());
     }
 
+    /**
+     * This method will test if the content is correct.
+     */
     @Test
     public void contentTest()
     {
         assertEquals("This is the first test message.", message.getContent());
     }
 
+    /**
+     * This method will test if the boolean value is the same.
+     */
     @Test
     public void sentFromPhoneTest()
     {
         assertEquals(true, message.isSentFromThisPhone());
     }
 
+    /**
+     * This method will test if the year returned is the correct one.
+     */
     @Test
     public void yearTest()
     {
