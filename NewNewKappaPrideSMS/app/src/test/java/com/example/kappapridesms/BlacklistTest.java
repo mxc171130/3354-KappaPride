5b100e442f44847c22410cb17225926a0c6cb78c
Alazar Debello <ahd170130@utdallas.edu>
2019-12-04 20:41:12 -0600

    Add BlacklistTest
    Added unit tests for the Blacklist class

---
+++ NewNewKappaPrideSMS/app/src/test/java/com/example/kappapridesms/BlacklistTest.java
@@ -0,0 +1,86 @@
+package com.example.kappapridesms;
+
+import android.app.PendingIntent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Testing of The Blacklist Class
+ * Statement Coverage 100%
+ * Branch Coverage 100%
+ * Path Coverage 100%
+ * @author AlazarDebello
+ */
+public class BlacklistTest {
+    private ArrayList<Long> m_blacklist=null;
+    @Before
+    public void setUp() throws Exception
+    {
+        m_blacklist= new ArrayList<>();
+
+    }
+
+    /**
+     * Testing the add function for the blacklist class
+     */
+    @Test
+    public void test1()
+    {
+        long num1=Long.parseLong("2222222222");
+        long num2=Long.parseLong("333333333");
+        m_blacklist.add(num1);
+        m_blacklist.add(num2);
+        assertEquals(2,m_blacklist.size());
+
+
+
+    }
+
+    /**
+     * Testing the remove function of the Blacklist class by removing a number that exist in the blacklist
+     *
+     */
+    @Test
+    public void test2()
+    {   long num1=Long.parseLong("222222222");
+        long num2=Long.parseLong("333333333");
+        m_blacklist.add(num1);
+        m_blacklist.add(num2);
+        m_blacklist.remove(num1);
+        assertEquals(1,m_blacklist.size());
+
+    }
+
+    /**
+     * Testing the remove function of the Blacklist class by removing a number that is not in the blacklist
+     */
+    @Test
+    public void test3()
+    {   long num1=Long.parseLong("2222222222");
+        long num2=Long.parseLong("3333333333");
+        m_blacklist.add(num1);
+        m_blacklist.add(num2);
+        m_blacklist.remove("2222222223");
+        assertEquals(2,m_blacklist.size());
+
+    }
+
+    /**
+     * Testing the getMethod within the BlackList class
+     */
+    @Test
+    public void test4()
+    {
+        long num1=Long.parseLong("222222222");
+        m_blacklist.add(num1);
+        long num2=m_blacklist.get(0);
+        assertEquals(num1,num2);
+    }
+
+
+}
